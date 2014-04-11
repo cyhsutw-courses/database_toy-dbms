f2425206c9918a47395cc4e7ccb9af24493553cf
ieo <pavel22621419@gmail.com>
2014-04-11 21:17:59 +0800

    QQQ

--- src/main/java/nihongo/chiisaidb/storage/TableScan.java
+++ src/main/java/nihongo/chiisaidb/storage/TableScan.java
@@ -14,35 +14,34 @@ import nihongo.chiisaidb.type.Constant;
 public class TableScan {
 	private TableInfo ti;
 	private RecordFile rf;
-	
+
 	public TableScan(String tblName) {
 		this.ti = Chiisai.mdMgr().getTableInfo(tblName);
 		this.rf = new RecordFile(this.ti);
 	}
 
 	public void insert(List<Constant> values) throws IOException {
-		
+
 		int fieldNum = values.size();
 		for (int i = 0; i < fieldNum; i++) {
 			rf.setVal(ti.schema().fieldNames().get(i), values.get(i));
 		}
 		rf.updateNumberOfRecordsBy(1);
-		
+
 	}
 
 	public Set<Object> getPrimaryKeyValueSet() throws Exception {
 		Set<Object> pkvs = new HashSet<Object>();
 		Set<Integer> pkfis = ti.schema().getPrimaryKeyFieldIndexes();
-		
-		
+
 		rf.beforeFirst();
-		
+
 		Constant val;
-		while(rf.next()){
-			for(int i=0; i<ti.schema().fieldNames().size(); i++){
+		while (rf.next()) {
+			for (int i = 0; i < ti.schema().fieldNames().size(); i++) {
 				String fldName = ti.schema().fieldNames().get(i);
 				val = rf.getVal(fldName);
-				if(pkfis.contains(i)){
+				if (pkfis.contains(i)) {
 					pkvs.add(val.getValue());
 				}
 			}
@@ -50,16 +49,24 @@ public class TableScan {
 		return pkvs;
 	}
 
+	public Constant getValue(String fldName) throws Exception {
+		return rf.getVal(fldName);
+	}
+
+	public boolean next() throws Exception {
+		return rf.next();
+	}
+
 	public void showAllRecord() throws Exception {
-		
+
 		System.out.print(ti.schema().toString());
 		System.out.println(rf.numberOfRecords() + " records");
 		System.out.println();
-		
+
 		rf.beforeFirst();
 		Constant val;
-		while(rf.next()){	
-			for(String fldName : ti.schema().fieldNames()){
+		while (rf.next()) {
+			for (String fldName : ti.schema().fieldNames()) {
 				val = rf.getVal(fldName);
 				System.out.print(val.getValue() + ", ");
 			}
@@ -69,7 +76,7 @@ public class TableScan {
 				System.out.print("[" + iterator.next() + "] ");
 			}
 			System.out.println();
-			
+
 		}
 	}
 }
